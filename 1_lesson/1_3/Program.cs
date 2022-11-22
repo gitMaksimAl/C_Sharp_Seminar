54b8e5ab65e2536e8154ef1518c43356a41084b7
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-22 19:36:01 +0400

    Task 3 - solved

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу которая на вход принимает одно число
+// (N) а на выходе показывает все числа от -N до N
+Console.WriteLine("Enter num>");
+int n = int.Parse(Console.ReadLine());
+
+int minorn = -n;
+while (minorn <= n) Console.Write(minorn++ + ", ");
