250e79d05a238b15f08301e5a45cf8119cfdb394
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-22 19:00:08 +0400

    Tasks 1,2 - solved

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Task 1
+// Напишите программу которая на вход принимает два числа и проверяет является
+// ли первое квадратом второго
+
+Console.WriteLine("Which num you want to ckeck");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("is power of");
+int y = int.Parse(Console.ReadLine());
+
+if (y * y == x) Console.WriteLine("YES");
+else Console.WriteLine("No");
+
+// Task 2
+// Напишите программу которая будет выдавать название дня недели по
+// заданному номеру
+
+Console.WriteLine("Enter num in range 1 - 7");
+x = int.Parse(Console.ReadLine());
+if (x == 1) Console.WriteLine("Monday");
+else if (x == 2) Console.WriteLine("Tuesday");
+else if (x == 3) Console.WriteLine("Wednesday");
+else if (x == 4) Console.WriteLine("Thursday");
+else if (x == 5) Console.WriteLine("Friday");
+else if (x == 6) Console.WriteLine("Saturday");
+else if (x == 7) Console.WriteLine("Sunday");
+else Console.WriteLine("WARNNG!!! Wrong num.");
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# C_Sharp_Seminar
+![C_Sharp logo](https://images.app.goo.gl/6wQYBgwsMry8VcLw8)
+# C_Sharp Seminar
+
+## Lesson 1
+Using input, output console methods, type cast
