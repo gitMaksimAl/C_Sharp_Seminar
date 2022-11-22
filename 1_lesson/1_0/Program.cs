117a8d5d669beaf78deeec8d733b6aa5aee202d6
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-22 18:25:11 +0400

    Task 1

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат
+Console.WriteLine("Enter the num >");
+
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine(x * x);
