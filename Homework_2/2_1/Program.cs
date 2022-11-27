e7b250bbd6674b6e640e3b885777a74d064e8505
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-27 18:00:15 +0400

    Homework lesson2, task 1 - solved

---
+++ Homework_2/2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру
+
+// function wait three-digit number from console
+int getThreeDig() {
+        int x = int.Parse(Console.ReadLine());
+        while (x < 100 || x > 999) {
+            Console.WriteLine("Try again.");
+            x = int.Parse(Console.ReadLine());
+        }
+        return x;
+}
+
+// function parse the three-digit number and output a second
+void printSecond(int n) {
+    n = n / 10;
+    Console.WriteLine(n % 10);
+}
+
+Console.WriteLine("Please enter three-digit number>");
+printSecond(getThreeDig());
