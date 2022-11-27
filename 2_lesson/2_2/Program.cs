cfd19d0d1d9dcaa39cc1bcfa01d0763f031b65af
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-27 16:37:18 +0400

    tasks2, 3, 4 solve

---
+++ 2_lesson/2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая на вход будет принимать два числа и выводить
+// является ли второе число кратным первому. Если второе число не кратно первому
+// программа выводит остаток от деления.
+
+void show_remainder(int divisor, int num) {
+        if (num % divisor == 0) Console.WriteLine($"{num} is evenly divisible");
+        else Console.WriteLine($"Remainder: {num % divisor}");
+}
+
+Console.WriteLine("Please enter two nums>");
+show_remainder(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ## Lesson 1
 Using input, output console methods, string conversion, formatted strings.
+
+## Lesson 2
+Using functions, composite statement, work with integers and range of integer.
