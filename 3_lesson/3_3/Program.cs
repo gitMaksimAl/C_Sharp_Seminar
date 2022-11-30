21c585af3607ca0662ea8d75d8b93f85757ee6cc
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-30 19:12:33 +0400

    task 4 - solved

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая принимает на вход число N и выдает таблицу квадратов чисел
+// от 1 до N
+
+void Pow_to(int n) {
+    int i = 1;
+    while (i <= n) {
+        Console.Write($"{i * i}, ");
+        i++;
+    }
+}
+
+Console.WriteLine("Enter positive num>");
+Pow_to(int.Parse(Console.ReadLine()));
