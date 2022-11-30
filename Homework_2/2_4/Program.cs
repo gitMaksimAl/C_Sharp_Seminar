a7ad1640fcd391a9404492a400fd2523bcb79fd4
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-30 08:38:00 +0400

    task 4 - solved

---
+++ Homework_2/2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход цифру обозначающую
+// день недели и проверяет является ли этот день выходным
+
+// function return Yes if the day is a holiday and No if not
+string isWeekend(int day) {
+    if (day > 0 && day < 6) return "Нет";
+    else if (day > 0 && day < 8) return "Да";
+    else return "В неделе 7 дней!!!";
+}
+
+Console.WriteLine("Please enter number of the day>");
+Console.WriteLine(isWeekend(int.Parse(Console.ReadLine())));
