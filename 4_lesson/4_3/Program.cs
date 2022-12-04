943a51fbf81a6afc3ab8d3223cf40e67fb60c9b4
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-12-04 10:48:25 +0400

    task 4 - solved, update README

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая выводит массив из восьми элементов
+// заполненый нулями и еденицами в случайном порядке
+
+void print_massive(int n) {
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new Random().Next(2);
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("");
+}
+
+Console.WriteLine("Enter nunber '8'>");
+print_massive(int.Parse(Console.ReadLine()));
