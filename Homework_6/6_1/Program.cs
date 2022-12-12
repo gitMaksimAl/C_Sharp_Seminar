0727ddf5ae39470825da27d7532f07811e8e73b6
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-12-12 16:47:22 +0400

    Homework 6 Task 1 - solved, update README

---
+++ Homework_6/6_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте сколько чисел больше 0 ввел пользователь.
+
+// read lines from console and counts the first num
+// greater then zero, while line is not empty
+int readNums() {
+    string line = System.String.Empty;
+    int num;
+    int count = 0;
+    line = Console.ReadLine();
+    while (line != "") {
+        if (line[0] > 48 && line[0] < 58) count++;
+        line = Console.ReadLine();
+    }
+    return count;
+}
+
+Console.WriteLine($" -> {readNums()}");
